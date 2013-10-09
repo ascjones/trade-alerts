8dac04ab4f6a02053cd472f5c964604e6a3d3fb8
Andrew Jones <ascjones@gmail.com>
2013-10-09 16:50:05 +0200

    converted spaces back to tabs and implemented multiple entries per file

--- pl.py
+++ pl.py
@@ -2,38 +2,56 @@ import quopri
 import html.parser
 import mailbox
 import re
+import pdb
 
 def get_email_body(email_message):
-    def toutf(part):
-        charset = part.get_content_charset() if part.get_content_charset() is not None else 'utf-8'
-        s = part.get_payload(decode=True)
-        if part.get_content_type() == "text/html":
-            h = html.parser.HTMLParser()
-            return h.unescape(str(s, charset))
-        else:
-            return str(quopri.decodestring(s), charset)
-    maintype = email_message.get_content_maintype()
-    if maintype == 'multipart':
-        for part in email_message.get_payload():
-            if part.get_content_maintype() == 'text':
-                return toutf(part)
-            elif maintype == 'text':
-                return toutf(email_message)
-    else:
-        return toutf(email_message)
+	def to_utf(part):
+		charset = part.get_content_charset() if part.get_content_charset() is not None else 'utf-8'
+		s = part.get_payload(decode=True)
+		if part.get_content_type() == "text/html":
+			h = html.parser.HTMLParser()
+			return h.unescape(str(s, charset))
+		else:
+			return str(quopri.decodestring(s), charset)
 
-regex = re.compile(r'.*(?P<direction>long|shorted) (?P<instrument>[A-Za-z/]*) @ (?P<entry>[0-9]*[.,]?[0-9]+).*protective stop @ (?P<stop>[0-9]*[.,]?[0-9]+)')
+	maintype = email_message.get_content_maintype()
+	if maintype == 'multipart':
+		for part in email_message.get_payload():
+			if part.get_content_maintype() == 'text':
+				return to_utf(part)
+			elif maintype == 'text':
+				return to_utf(email_message)
+	else:
+		return to_utf(email_message)
 
-def parse_trade_alerts(msg):
-	def pricestr_toint(price):
-		return int(price.replace(',', ''))
-	multi_move_stop_match = re.search(r'TRADE ALERT\n([A-Z/]*)(?:\sAND\s)?([A-Z/]*)\n.*moving protective stops.*to ([0-9]*[.,]?[0-9]+) and ([0-9]*[.,]?[0-9]+)', msg, re.IGNORECASE)
+
+regex = re.compile(r'.*(?P<direction>long|short) (?P<instrument>[A-Z/]*) @ (?P<entry>[0-9]*[.,]?[0-9]+).*protective stop @ (?P<stop>[0-9]*[.,]?[0-9]+)')
+
+
+def parse_trade_alert(msg):
+	def price_to_pips(price):
+		# remove dots and commas to turn large numbers and decimals into pips
+		stripped = re.sub(r'(\d+)[,.](\d+)', r'\1\2', price)
+		return int(stripped)
+
+	multi_move_stop_match = re.search(
+		r'([A-Z/]*)(?:\sAND\s)?([A-Z/]*)\n.*moving protective stops.*to ([0-9]*[.,]?[0-9]+) and ([0-9]*[.,]?[0-9]+)',
+		msg, re.IGNORECASE)
+
+	commands = []
+
 	if multi_move_stop_match:
 		groups = multi_move_stop_match.groups()
-		movestop1 = MoveStopTradeAlert(groups[0], 'SHORT', pricestr_toint(groups[2]))
-		movestop2 = MoveStopTradeAlert(groups[1], 'SHORT', pricestr_toint(groups[3]))
-		return [movestop1, movestop2]
-	return []
+		commands.append(MoveStop(groups[0], 'SHORT', price_to_pips(groups[2])))
+		commands.append(MoveStop(groups[1], 'SHORT', price_to_pips(groups[3])))
+
+	for match in regex.finditer(msg):
+		price = price_to_pips(match.group('entry'))
+		stop = price_to_pips(match.group('stop'))
+		commands.append(OpenTrade(match.group('instrument'), match.group('direction'), price, stop))
+
+	return commands
+
 
 maildir = mailbox.Maildir('/Users/andrew/.getmail/trade-alerts')
 
@@ -41,12 +59,13 @@ for msg in maildir:
 	body = get_email_body(msg)
 	match = regex.search(body)
 	print(body)
-	# if match:
-	# 	match.groupdict()
-	# else:
-	# 	print(body)
+# if match:
+# 	match.groupdict()
+# else:
+# 	print(body)
 
-class OpenTradeAlert:
+
+class OpenTrade:
 	def __init__(self, instrument, direction, price, stop):
 		self.instrument = instrument
 		self.direction = direction
@@ -56,7 +75,8 @@ class OpenTradeAlert:
 	def apply(self, trades):
 		trades.append(Trade(self.instrument, self.direction, self.price, self.stop))
 
-class CloseTradeAlert:
+
+class CloseTrade:
 	def __init__(self, instrument, price, account):
 		self.instrument = instrument
 		self.price = price
@@ -66,11 +86,13 @@ class CloseTradeAlert:
 		trade = next((t for t in trades if t.instrument == self.instrument), None)
 		trade.close(self.price)
 
-class MoveStopTradeAlert:
+
+class MoveStop:
 	def __init__(self, instrument, direction, stop):
 		self.instrument = instrument
 		self.direction = direction
 		self.stop = stop
+
 
 class Trade:
 	def __init__(self, instrument, direction, opening, stop):
@@ -89,19 +111,19 @@ class Trade:
 	def pl(self):
 		return self.closing - self.opening
 
+
 class BackTestResult:
 	pass
 
-class BackTest:
 
+class BackTest:
 	def run(self, trade_alerts):
-		trades = list()	
+		trades = list()
 		for alert in trade_alerts:
-			alert.apply(trades) 
+			alert.apply(trades)
 		result = BackTestResult()
 		result.pips = sum([t.pl() for t in trades])
 		return result
-	
 
 
 
--- pl_test.py
+++ pl_test.py
@@ -3,22 +3,22 @@ from pl import *
 class TestParseTradeAlerts:
 	def test_double_trade(self):
 		body = 'TRADE ALERT\nDOW AND FTSE\nI am moving protective stops on shorts to 15,298 and 6648 resp.\nUSD/JY\nI have just gone long USD/JY @ 97.20 with protective stop @ 96.20 for\na 100 pip risk.\nJohn\n\n\n'
-		alerts = parse_trade_alerts(body)
-		assert len(alerts) == 2
-		assert isinstance(alerts[0], MoveStopTradeAlert)
-		assert isinstance(alerts[1], MoveStopTradeAlert)
+		commands = parse_trade_alert(body)
+		assert len(commands) == 3
+		assert isinstance(commands[0], MoveStop)
+		assert isinstance(commands[1], MoveStop)
 
 class TestBackTest:
 	def test_long_trade_profit(self):
-		opentrade = OpenTradeAlert('GBP/USD', 'LONG', 15210, 15160)
-		closetrade = CloseTradeAlert('GBP/USD', 15310, 'A')
+		opentrade = OpenTrade('GBP/USD', 'LONG', 15210, 15160)
+		closetrade = CloseTrade('GBP/USD', 15310, 'A')
 		backtest = BackTest()
 		pl = backtest.run([opentrade, closetrade])
 		assert pl.pips == 100
 
 	def test_long_trade_loss(self):
-		opentrade = OpenTradeAlert('GBP/USD', 'LONG', 15210, 15160)
-		closetrade = CloseTradeAlert('GBP/USD', 15200, 'A')
+		opentrade = OpenTrade('GBP/USD', 'LONG', 15210, 15160)
+		closetrade = CloseTrade('GBP/USD', 15200, 'A')
 		backtest = BackTest()
 		pl = backtest.run([opentrade, closetrade])
 		assert pl.pips == -10	
