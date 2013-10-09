628961a46fe353c02dc1579fd9e5351b2022def3
Andrew Jones <ascjones@gmail.com>
2013-10-09 11:43:33 +0200

    calculate profit/loss for basic trade alerts

--- pl.py
+++ pl.py
@@ -35,9 +35,62 @@ maildir = mailbox.Maildir('/Users/andrew/.getmail/trade-alerts')
 for msg in maildir:
 	body = get_email_body(msg)
 	match = regex.search(body)
-	if match:
-		print(match.groupdict())
-	else:
-		print('no match')
+	print(body)
+	# if match:
+	# 	match.groupdict()
+	# else:
+	# 	print(body)
+
+class OpenTradeAlert:
+	def __init__(self, instrument, direction, price, stop):
+		self.instrument = instrument
+		self.direction = direction
+		self.price = price
+		self.stop = stop
+
+	def apply(self, trades):
+		trades.append(Trade(self.instrument, self.direction, self.price, self.stop))
+
+class CloseTradeAlert:
+	def __init__(self, instrument, price, account):
+		self.instrument = instrument
+		self.price = price
+		self.account = account
+
+	def apply(self, trades):
+		trade = next((t for t in trades if t.instrument == self.instrument), None)
+		trade.close(self.price)
+
+class Trade:
+	def __init__(self, instrument, direction, opening, stop):
+		self.instrument = instrument
+		self.direction = direction
+		self.opening = opening
+		self.stop = stop
+		self.closing = None
+
+	def close(self, price):
+		self.closing = price
+
+	def isopen(self):
+		return self.closing == None
+
+	def pl(self):
+		return self.closing - self.opening
+
+class BackTestResult:
+	pass
+
+class BackTest:
+
+	def run(self, trade_alerts):
+		trades = list()	
+		for alert in trade_alerts:
+			alert.apply(trades) 
+		result = BackTestResult()
+		result.pips = sum([t.pl() for t in trades])
+		return result
+	
+
 
 
--- pl_test.py
+++ pl_test.py
@@ -1,6 +1,14 @@
-import pl
+from pl import *
 
 def test_double_trade():
 	body = 'TRADE ALERT\nDOW AND FTSE\nI am moving protective stops on shorts to 15,298 and 6648 resp.\nUSD/JY\nI have just gone long USD/JY @ 97.20 with protective stop @ 96.20 for\na 100 pip risk.\nJohn\n\n\n'
-	trades = pl.parse_trades(body)
+	trades = parse_trades(body)
 	assert len(trades) == 2
+
+def test_long_gbpusd_profit():
+	go_long = OpenTradeAlert('GBP/USD', 'LONG', 15210, 15160)
+	take_profit = CloseTradeAlert('GBP/USD', 15310, 'A')
+	backtest = BackTest()
+	pl = backtest.run([go_long, take_profit])
+	assert pl.pips == 100
+
