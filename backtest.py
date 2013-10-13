ead8b58eda34d02e61644a20420ea7390732839c
Andrew Jones <ascjones@gmail.com>
2013-10-13 17:17:04 +0200

    print p/l per instrument and filter trades per instrument

--- backtest.py
+++ backtest.py
@@ -1,5 +1,7 @@
 from model import *
 import alerts
+from collections import defaultdict
+import sys
 
 class BackTestResult:
 	def __init__(self, trades, accounts):
@@ -9,6 +11,13 @@ class BackTestResult:
 	def account_pl(self, acc_name):
 		acc = [a for a in self.accounts if a.name == acc_name][0]
 		return acc.pips
+
+	def instruments_pl(self):
+		pl_by_instr = defaultdict(lambda: defaultdict(lambda: 0))
+		for t in self.trades:
+			for acc, pl in t.pl().items():
+				pl_by_instr[t.instrument][acc] += pl
+		return pl_by_instr
 
 	def total_pl(self):
 		return sum([a.pips for a in self.accounts])
@@ -22,12 +31,16 @@ class BackTestResult:
 		def perc(trade_count):
 			return (trade_count / total) * 100
 
-		return r''' 
+		result = ''' 
 Trades: {}
 Long:   {} ({}%)
 Short:  {} ({}%)
-P/L: A:{}, B:{}, C:{}, Total: {}'''.format(
+P/L: A:{}, B:{}, C:{}, Total: {}\n\n'''.format(
 	total, longs, perc(longs), shorts, perc(shorts), self.account_pl('A'), self.account_pl('B'), self.account_pl('C'), self.total_pl())
+
+		result += '\n'.join(['{} - {}'.format(instr, ','.join('{}:{}'.format(acc, pl) for acc, pl in pls.items())) for instr, pls in self.instruments_pl().items()])
+		return result
+
 
 class BackTest:
 	def __init__(self):
@@ -39,17 +52,13 @@ class BackTest:
 			if instruments == None or alert.instrument in instruments:
 				alert.apply(trades, self.accounts)
 
-		# accounts_pl = dict({'A': 0, 'B': 0, 'C': 0})
-		# for trade in trades:
-		# 	for acc, pl in accounts_pl:
-		# 		if acc in trade.closing_prices.keys()
-		# 			accou
 
 		return BackTestResult(trades, self.accounts)
 
 if __name__ == '__main__':
 	backtest = BackTest()
-	result = backtest.run(alerts.trade_alerts) #, ['USD/JY'])
+	instr = sys.argv[1] if len(sys.argv) == 2 else None
+	result = backtest.run(alerts.trade_alerts, instr)
 	print(result)
 
 	print('================================================================================\n')
